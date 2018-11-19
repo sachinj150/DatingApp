89b889d93fdbeb7ce4b2b271e444713b04c929ae
Jambur <sjambur@deloitte.ca>
2018-11-19 11:15:38 -0700

    Error Handling

--- DatingApp-SPA/src/app/_services/error.interceptor.ts
+++ DatingApp-SPA/src/app/_services/error.interceptor.ts
@@ -9,11 +9,25 @@ export class ErrorInterceptor implements HttpInterceptor {
         return next.handle(req).pipe(
             catchError(error => {
                 if (error instanceof HttpErrorResponse) {
+                    if (error.status === 401) {
+                        return throwError(error.statusText);
+                    }
                     const applicationError = error.headers.get('Application-Error');
                     if (applicationError) {
                         console.error(applicationError);
                         return throwError(applicationError);
                     }
+
+                    const serverError = error.error;
+                    let modalStateErrors = '';
+                    if (serverError && typeof serverError === 'object') {
+                        for (const key in serverError) {
+                            if (serverError[key]) {
+                                modalStateErrors += serverError[key] + '\n';
+                            }
+                        }
+                    }
+                    return throwError(modalStateErrors || serverError || 'Server Error');
                 }
             })
         );
